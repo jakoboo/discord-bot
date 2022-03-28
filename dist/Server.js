d18df03f1f9e28effbcd767b5af94f7e796b96cf
Jakub Bąbelek <jbbabelek@gmail.com>
2022-03-28 18:06:00 +0200

    Added dist folder to gitignore

--- dist/Server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Server {
-    guild;
-    constructor(guild) {
-        this.guild = guild;
-    }
-}
-exports.default = Server;
